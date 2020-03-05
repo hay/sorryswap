e4c9f23a33982406973a386fbad189490be1c75b
Hay Kranen <huskyr@gmail.com>
2020-03-05 15:29:30 +0100

    Adding karaoke text

---
+++ app/js/components/el-textbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="el-textbar">
+        <div class="el-textbar__textwrapper">
+            <div v-bind:style="style"
+                 class="el-textbar__text"
+                 ref="text">{{text}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                style : {
+                    'transition-duration' : this.duration + 's',
+                    transform : ''
+                },
+                textHeight : 0
+            }
+        },
+
+        methods : {
+            run() {
+                const transform = 'translateY(-' + this.textHeight + 'px)';
+                this.style.transform = transform;
+            }
+        },
+
+        mounted() {
+            const box = this.$refs.text.getBoundingClientRect();
+            this.textHeight = box.height;
+        },
+
+        props : {
+            duration : {
+                type : Number
+            },
+
+            text : {
+                type : String
+            }
+        }
+    }
+</script>
--- app/js/components/step-record.vue
+++ app/js/components/step-record.vue
@@ -1,33 +1,44 @@
 <template>
     <div class="recorder__step">
         <div class="recorder__content">
-            <div class="recorder__panels recorder__panels--vertical">
-                <section class="recorder__panel">
-                    <div class="progress"
-                         ref="progress"
-                         v-bind:running="recording"></div>
-                    <video ref="video" autoplay playsinline></video>
-                </section>
+            <div class="el-recorder">
+                <div class="el-recorder__progress"
+                     ref="progress"
+                     v-bind:running="recording"></div>
 
-                <div class="recorder__panel">
-                    <section class="recorder__text recorder__text--small">
-                        <p v-html="script.text"></p>
-                    </section>
+                <video
+                    class="el-recorder__video"
+                    ref="video" autoplay playsinline></video>
+
+                <div class="el-recorder__gifs">
+                    <img src="/static/img/cat1.gif" />
+                    <img src="/static/img/cat2.gif" />
+                    <img src="/static/img/duck1.gif" />
+                    <img src="/static/img/duck2.gif" />
                 </div>
+
+                <el-textbar
+                    ref="textbar"
+                    v-bind:duration="textbarDuration"
+                    v-bind:text="script.text"></el-textbar>
             </div>
 
             <div v-if="count > 0" class="counter">
                 <span>{{count}}</span>
             </div>
-        </div>
     </div>
 </template>
 
 <script>
+    import ElTextbar from './el-textbar.vue';
     import Recorder from '../recorder.js';
     import { timeout } from '../util.js';
 
     export default {
+        components : {
+            ElTextbar
+        },
+
         computed : {
             script() {
                 return this.$store.getters.targetScript;
@@ -39,7 +50,8 @@
                 count : 0,
                 recorder : null,
                 recording : false,
-                recordingTime : this.$store.state.recordingTime
+                recordingTime : this.$store.state.recordingTime,
+                textbarDuration : this.$store.state.conf.global.output_video_time
             }
         },
 
@@ -87,7 +99,9 @@
         async mounted() {
             this.$refs.progress.style.transitionDuration = this.recordingTime + 's';
             this.setupRecorder();
+            await timeout(2000);
             await this.startCount();
+            this.$refs.textbar.run();
             this.start();
             await timeout(this.recordingTime * 1000);
             this.stop();
