df3b73d8dea5a54372bf60a2e0e3dae63762c579
Hay Kranen <huskyr@gmail.com>
2020-03-27 13:51:37 +0100

    Adding new gifs and animation in splash screen

--- app/js/components/step-splash.vue
+++ app/js/components/step-splash.vue
@@ -2,6 +2,14 @@
     <div class="step splash"
          flair="blue">
 
+        <video class="splash__gif"
+               autoplay
+               loop
+               muted
+               playsinline
+               ref="gif"
+               v-bind:src="splashGif"></video>
+
         <el-image src="face.png"></el-image>
 
         <el-image src="logo.png"></el-image>
@@ -16,8 +24,39 @@
 </template>
 
 <script>
+    import anime from 'animejs';
+    import { timeout } from '../util.js';
+
+    const GIF_WIDTH = 496;
+    const GIF_HEIGHT = 288;
+    let gifX = '100vw';
+
     export default {
+        data() {
+            return {
+                'splashGif' : 'img/gif-1.mp4'
+            }
+        },
+
         methods : {
+            async moveSplashGif() {
+                anime({
+                    targets : this.$refs.gif,
+                    translateX : gifX,
+                    translateY : 'calc(50vh - 144px)',
+                    rotate : ['0deg', '20deg', '-20deg', '0deg'],
+                    scale : [1, 1.5, 1],
+                    duration : 10000,
+                    easing : 'linear',
+                    complete: () => {
+                        this.splashGif = `img/gif-${anime.random(1, 5)}.mp4`;
+                        gifX = gifX === '-50vw' ? '100vw' : '-50vw';
+                        timeout(2000);
+                        this.moveSplashGif();
+                    }
+                });
+            },
+
             start() {
                 this.$sounds.play('transform');
                 this.$music.play('placeholder');
@@ -29,6 +68,7 @@
         mounted() {
             this.$socket.emit('recorder', 'splash');
             this.$keys.on('enter', this.start.bind(this));
+            this.moveSplashGif();
         }
     }
 </script>
